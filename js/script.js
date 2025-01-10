a9fc5ab47c9d9049349f3ff0eda3d56b3ba4cc18
Erremme <Erremme2.0@gmail.com>
2025-01-10 12:27:58 +0100

    feat: add overlay and event click

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
   .then(function (res) {
-    // handle success
-    console.log(res.data)
     const dataObj = res.data
 
     dataObj.forEach(card => {
         const titleCard = card.title
         const urlCard = card.url
         cardContainElm.innerHTML += `
-                                    <div class="col-6 col-sm-4 col-lg-3  mb-5 ">
+                                    <div class=" ms-col col-6 col-sm-4 col-lg-3  mb-5 ">
                                         <div class="ms-card">
                                             <img src="./img/pin.svg" alt="">
                                             <div class="ms-card-img">
                                                 <img src="${urlCard}" alt="">
-                                        </div>
+                                             </div>
                         
                                         <div class="ms-card-text">
                                             <p class="m-0">${titleCard}</p>
@@ -21,12 +19,25 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
                                     </div>
                                 </div>`
     });
+    
+    const cards = document.querySelectorAll(".ms-card-img img")
 
-    
-    /*const names = dataObj.map(dataObj => dataObj.title);
-    console.log(names);*/
-    
+    cards.forEach(card => {
+      card.addEventListener("click", function(){
+        const imgZoom = document.getElementById("overlay-img").src = `${card.src}`;
+        console.log(imgZoom)
+          overlayContainElm.classList.remove("d-none")
+      })
+    });
+
+
   })
 
 //DOM ELEMENT
 const cardContainElm = document.getElementById("card-contain") 
+const overlayContainElm = document.getElementById("overlay-container") 
+const btnCloseElm = document.getElementById("btn-close") 
+
+btnCloseElm.addEventListener("click", function(){
+  overlayContainElm.classList.add("d-none")
+})
